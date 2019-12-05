6f6bce010b0b37f3a91195403fb3795c2480f738
Bogdan 'TiBx' Savenko <savenko.bogdan@gmail.com>
2019-12-05 02:09:37 +0200

    added matrix class

--- matrix.cpp
+++ matrix.cpp
@@ -3,9 +3,27 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
+
+template<typename T, T value>
+class Matrix
+{
+public:
+    T& operator[](std::size_t index)
+    {
+        return m_values[index];
+    }
+
+private:
+    std::map<std::size_t, T> m_values;
+};
 
 int main(/*int argc, char const *argv[]*/)
 {
+    Matrix<int, -1> m;
+    m[0] = 10;
+    std::cout << m[0] << "\t" << m[1] << std::endl;
+
     return 0;
 }
