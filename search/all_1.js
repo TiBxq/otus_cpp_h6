26556e18851791f48578b0ea3af26123912f6897
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-05 00:13:03 +0000

    Deploy TiBxq/otus_cpp_h6 to github.com/TiBxq/otus_cpp_h6.git:gh-pages

--- search/all_1.js
+++ search/all_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['otus_5fcpp_5fh6',['otus_cpp_h6',['../md_README.html',1,'']]]
+  ['otus_5fcpp_5fh6',['otus_cpp_h6',['../md_README.html',1,'']]],
+  ['operator_5b_5d',['operator[]',['../class_matrix.html#a07689bbd5459f62c65313a545f30d70c',1,'Matrix']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['test',['TEST',['../matrix__test_8cpp.html#acc50c2895748370ecd21b3fba7def8ca',1,'matrix_test.cpp']]]
+  ['operator_5b_5d',['operator[]',['../class_matrix.html#a07689bbd5459f62c65313a545f30d70c',1,'Matrix']]]
 ];
