2b7a2439fbe87b0b729d77a703fdf4925134d7b4
Bogdan Savenko <savenko.bogdan@gmail.com>
2019-12-05 19:06:20 +0200

    added MatrixElement

--- matrix.cpp
+++ matrix.cpp
@@ -6,24 +6,73 @@
 #include <map>
 #include <algorithm>
 
-template<typename T, T value>
+template<typename T>
+class Matrix;
+
+template<typename T>
+class MatrixElement
+{
+public:
+    MatrixElement() : m_matrix(nullptr), m_index(-1) {}
+    MatrixElement(T value, int index, Matrix<T>* matrix) : m_value(value), m_index(index), m_matrix(matrix) {}
+    T get() { return m_value; }
+    T& operator=(const T& value)
+    {
+        m_value = value;
+        m_matrix->Add(*this);
+        return m_value;
+    }
+//private:
+    T m_value;
+    int m_index;
+    Matrix<T>* m_matrix;
+};
+
+template<typename T>
+class MatrixElementProxy
+{
+public:
+    
+};
+
+template<typename T>
 class Matrix
 {
 public:
-    T& operator[](std::size_t index)
+    Matrix() : m_size( 0 ) {}
+
+    MatrixElement<T> operator[](std::size_t index)
     {
+        auto it = m_values.find(index);
+        if (it == m_values.end())
+        {
+          return MatrixElement<T>(-1, -1, this);
+        }
+
+        //if (index > m_size) m_size = index;
         return m_values[index];
     }
 
+    void Add(const MatrixElement<T>& value)
+    {
+      m_values.emplace(value.m_index, value);
+    }
+
 private:
-    std::map<std::size_t, T> m_values;
+    std::map<std::size_t, MatrixElement<T>> m_values;
+    std::size_t m_size;
+    //T m_default = value;
 };
 
 int main(/*int argc, char const *argv[]*/)
 {
-    Matrix<int, -1> m;
+    
+
+    Matrix<int> m;
+    //MatrixElement<int> a( -1, &m );
+    //a.
     m[0] = 10;
-    std::cout << m[0] << "\t" << m[1] << std::endl;
+    std::cout << m[0].get() << "\t" << m[1].get() << std::endl;
 
     return 0;
 }
